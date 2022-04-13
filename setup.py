f12472dcf7be35f65af150750e4650f6cd0af595
chomu37 <2chanhaeng@gmail.com>
2022-04-14 06:48:09 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bwj",
+    version="0.0.1",
+    description="Python 과 Jupyter Notebook 을 이용해 [백준 온라인 저지](https://www.acmicpc.net/)를 풀 때 도움을 줄 수 있는 모듈입니다.",
+    author="chomu",
+    author_email="2chanhaeng@gmail.com",
+    packages=["bwj"],
+    # install_requires=[],
+)
