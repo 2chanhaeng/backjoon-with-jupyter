ba07c8264a5e80b5a905c7690729fd90e41e43be
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-06-29 21:46:08 +0900

    Refactor test function
    이전에는 open 혹은 input 함수를 새로 지정해서 사용했으나, [박응용 님의 점프 투 파이썬 - 라이브러리 예제 편](https://wikidocs.net/book/5445) [059 문자열을 파일처럼 다루려면? ― io.StringIO](https://wikidocs.net/122776) 에서 문자열을 io.StringIO 클래스를 이용하면 파일 객체처럼 사용할 수 있다는 것을 알게 되어 예제를 io.StringIO 클래스로 변환하여 sys.stdin 로 등록시키도록 만들었습니다. 이제 문제 풀이 함수 내부에서 sys.stdin 을 호출하면 해당 예제를 실행할 수 있도록 만들었습니다.

--- bwj/__init__.py
+++ bwj/__init__.py
@@ -1,4 +1,5 @@
-
+import sys
+from io import StringIO
 from inspect import signature
 from typing import Any, Callable, Container, Iterator, Optional, Final
 
@@ -85,33 +86,9 @@ class Problem:
 
 
 def test(solution: Callable[[Callable], None]) -> Callable[[Optional[str]], None]:
-    def test_example(input_: Optional[str] = None) -> None:
-        params: Container[str] = signature(solution).parameters
-
-        if input_ == None:
-            solution()
-
-        elif "open" in params:
-
-            class open:
-                def __init__(self, _: Any):
-                    self.read: Callable = lambda: input_
-
-                def __iter__(self):
-                    return iter(input_.split("\n"))
-
-            solution(open)
-
-        elif "input" in params:
-            input_: Iterator[str] = iter(input_.split("\n"))
-            input: Callable[[None], str] = input_.__next__
-            solution(input)
-
-        else:
-            raise NameError(
-                f"solution 함수가 input, open 등의 매개변수를 받지 않습니다.\nsolution 의 매개변수: {params}"
-            )
-
+    def test_example(example: Optional[str] = None) -> None:
+        sys.stdin = StringIO(example)
+        solution()
     return test_example
 
 
