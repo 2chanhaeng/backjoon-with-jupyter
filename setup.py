96f5895ed55ec5677c03f7657a557db0f78fd61c
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-11 02:12:56 +0900

    Ready for register to PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name="bwj",
-    version="0.0.6",
-    description="Python 과 Jupyter Notebook 을 이용해 [백준 온라인 저지](https://www.acmicpc.net/)를 풀 때 도움을 줄 수 있는 모듈입니다.",
-    author="chomu",
-    author_email="2chanhaeng@gmail.com",
-    packages=["bwj"],
-    package_data={
-        "bwj": ["static/*.ipynb", "static/*.json"],
-    },
-    install_requires=["requests", "bs4"],
-)
+setuptools.setup()
