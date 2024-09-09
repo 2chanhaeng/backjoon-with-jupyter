5d2d673ca8d816e34fae5f521f592790ff1029dc
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-09-10 05:47:05 +0900

    Update 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="bwj",
-    version="0.0.6",
+    version="0.0.7",
     description="Python 과 Jupyter Notebook 을 이용해 [백준 온라인 저지](https://www.acmicpc.net/)를 풀 때 도움을 줄 수 있는 모듈입니다.",
     author="chomu",
     author_email="2chanhaeng@gmail.com",
